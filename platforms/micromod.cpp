9754c7090d4d90c45d245d9c8e64dc6a5cb4a31b
Khalil Estell <kammce0@gmail.com>
2024-07-08 14:37:51 -0700

    :adhesive_bandage: Testing and some minor updates

--- platforms/micromod.cpp
+++ platforms/micromod.cpp
@@ -21,10 +21,16 @@ hardware_map_t initialize_platform()
   using namespace hal::literals;
 
   hal::micromod::v1::initialize_platform();
+  auto& console = hal::micromod::v1::console(hal::buffer<128>);
+  console.configure(hal::serial::settings{
+    .baud_rate = 115200,
+    .stop = hal::serial::settings::stop_bits::one,
+    .parity = hal::serial::settings::parity::none,
+  });
 
   return {
     .red_led = &hal::micromod::v1::led(),
-    .console = &hal::micromod::v1::console(hal::buffer<128>),
+    .console = &console,
     .clock = &hal::micromod::v1::uptime_clock(),
     .can = &hal::micromod::v1::can(),
     .reset = +[]() { hal::micromod::v1::reset(); },
