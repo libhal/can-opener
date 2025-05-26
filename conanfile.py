7b9fbdbea98f5a55c0366221eb3427a865051bf8
Khalil Estell <kammce0@gmail.com>
2025-05-26 11:39:07 -0700

    :arrow_up: libhal-bootstrap/[^4.2.1]

--- conanfile.py
+++ conanfile.py
@@ -18,7 +18,7 @@ required_conan_version = ">=2.0.14"
 
 
 class demos(ConanFile):
-    python_requires = "libhal-bootstrap/[^3.0.0]"
+    python_requires = "libhal-bootstrap/[^4.2.1]"
     python_requires_extend = "libhal-bootstrap.demo"
 
     def requirements(self):
