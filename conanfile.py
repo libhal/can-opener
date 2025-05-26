2b778029a59c024ef4b4b279221ef40da23be220
Khalil Estell <kammce0@gmail.com>
2025-05-26 12:03:07 -0700

    :arrow_up: libhal-bootstrap/[^4.2.1] (#26)
    * :arrow_up: libhal-bootstrap/[^4.2.1]
    
    * :bug: Use correct API calls to can_message

--- conanfile.py
+++ conanfile.py
@@ -14,11 +14,11 @@
 
 from conan import ConanFile
 
-required_conan_version = ">=2.0.14"
+required_conan_version = ">=2.10.0"
 
 
 class demos(ConanFile):
-    python_requires = "libhal-bootstrap/[^3.0.0]"
+    python_requires = "libhal-bootstrap/[^4.2.1]"
     python_requires_extend = "libhal-bootstrap.demo"
 
     def requirements(self):
