8185421fbfacae415620374261b62c054cb26799
Khalil Estell <kammce0@gmail.com>
2025-05-27 15:09:30 -0700

    :pushpin: Use range for python_requires

--- conanfile.py
+++ conanfile.py
@@ -1,4 +1,4 @@
-# Copyright 2024 Khalil Estell
+# Copyright 2024 - 2025 Khalil Estell and the libhal contributors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -18,7 +18,7 @@ required_conan_version = ">=2.10.0"
 
 
 class demos(ConanFile):
-    python_requires = "libhal-bootstrap/[^4.2.1]"
+    python_requires = "libhal-bootstrap/[>=4.3.0 <5]"
     python_requires_extend = "libhal-bootstrap.demo"
 
     def requirements(self):
